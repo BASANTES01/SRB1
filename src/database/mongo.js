a4e4a0a8507469dc5d1a8583c7429784b8204665
BASANTES01 <141286674+BASANTES01@users.noreply.github.com>
2024-04-18 18:55:16 -0500

    ya termine
    ...

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const app = express();
-
-app.use(session({
-    secret: 'jeremybasantes',
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ 
-        mongoUrl: 'mmongmongosh "mongodb+srv://cluster0.juchyqr.mongodb.net/" --apiVersion 1 --username <username>odb+srv://jeremybasantes:12345@cluster0.juchyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0ngodb+srv://jeremybasantes3:1234@cluster0.juchyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-        
-    })
-}));
-
 
