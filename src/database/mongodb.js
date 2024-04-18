11973b7a963eefcc9448093ea30b09c818540829
BASANTES01 <141286674+BASANTES01@users.noreply.github.com>
2024-04-18 11:40:02 -0500

    nuevo

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,26 +1,10 @@
+const mongoose = require('mongoose');
+const URI="mongodb+srv://jeremybasantes3:1234@cluster0.juchyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//CONEXION A MONGODB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jeremybasantes3:1234@cluster0.juchyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(URI)
+  .then(() => console.log('¡Mongodb - Conectado!'))
+  .catch(err => console.error("Mongodb - Error al conectarse: ",err));
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//EXPORTANDO MODULO MONGOOSE
+module.exports = mongoose;
