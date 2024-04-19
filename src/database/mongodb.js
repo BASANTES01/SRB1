e3d1e695c92b33b9db450ef5c3c86d9de1732ea8
BASANTES01 <141286674+BASANTES01@users.noreply.github.com>
2024-04-18 19:39:07 -0500

    ta

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,14 +1,12 @@
-require('dotenv').config(); // Asegúrate de que esto está al principio de tu archivo principal
-
 const mongoose = require('mongoose');
 
-// Asumiendo que utilizas una variable de entorno llamada MONGO_URI
-const mongoURI = process.env.MONGO_URI;
+
+const mongoURI = process.env.MONGOURICLOUD;
 
 if (!mongoURI) {
-    console.error('La URI de MongoDB no está definida en las variables de entorno');
+    console.error('No se encontró la URI de MongoDB en las variables de entorno.');
 } else {
     mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('MongoDB conectado exitosamente'))
-        .catch(err => console.error('Error al conectar con MongoDB', err));
+        .then(() => console.log('Conectado a MongoDB.'))
+        .catch(err => console.error('Error al conectar con MongoDB:', err));
 }
